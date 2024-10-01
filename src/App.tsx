0621a150830fda34967bc8b7839633630f20cee0
Sebastian <sebasrojasm1@gmail.com>
2024-10-01 18:00:25 -0500

    Modified sidebar component and index.css styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import Sidebar from './components/sidebar'
 
 function App() {
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const handleFilterChange = (types: string[]) => {
+  const handleFilterChange = (types: string[], term: string) => {
     setSelectedTypes(types);
+    setSearchTerm(term);
   };
+  
   return (
     <>
       <Sidebar onFilterChange={handleFilterChange} />
-      <CardsPokemon selectedTypes={selectedTypes} />
+      <CardsPokemon selectedTypes={selectedTypes} searchTerm={searchTerm}/>
     </>
   )
 }
