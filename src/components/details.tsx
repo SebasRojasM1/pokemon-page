99686bce13c405dd271e5663999b27e8f6870362
Sebastian <sebasrojasm1@gmail.com>
2024-10-02 11:05:48 -0500

    The details component and styles was updated temporally

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -6,61 +6,65 @@ function Details() {
             <div className="pokemon-name">
                 <p>Pikachu #123</p>
             </div>
-            
+
             <div className="principal-info">
-                <div className="pokemon-image">
-                    <img src="https://pngimg.com/uploads/pokemon/small/pokemon_PNG9.png" alt="" />
+                <div className="pokemon-image-container">
+                    <div className="pokemon-image">
+                        <img src="https://pngimg.com/uploads/pokemon/small/pokemon_PNG9.png" alt="" />
+                    </div>
                 </div>
-                
-                <div className="pokemon-">
-                    <div className="type-pokemon">
-                        <p>Type</p>
-                        <div>
-                            <p>Rock</p>
-                            <p>Electric</p>
+
+                <div className="pokemon-profile-container">
+                    <div className="pokemon-profile">
+                        <div className="type-pokemon">
+                            <h3>Type</h3>
+
+                            <div className="types">
+                                <p>Rock</p>
+                                <p>Electric</p>
+                            </div>
+                        </div>
+
+                        <div className="height-pokemon">
+                            <h3>Height</h3>
+                            <p>2.2 m</p>
+                        </div>
+
+                        <div className="weight-pokemon">
+                            <h3>Weight</h3>
+                            <p>210.0 kg</p>
                         </div>
                     </div>
 
-                    <div className="height-pokemon">
-                        <p>Height</p>
-                        <p>2.2 m</p>
-                    </div>
+                    <div className="pokemon-special">
+                        <div className="specie-pokemon">
+                            <h3>Species</h3>
+                            <p>Dragon</p>
+                        </div>
 
-                    <div className="weight-pokemon">
-                        <p>Weight</p>
-                        <p>210.0 kg</p>
-                    </div>
-                </div>
-
-                <div>
-                    <div className="specie-pokemon">
-                        <p>Species</p>
-                        <p>Dragon</p>
-                    </div>
-
-                    <div className="abilities-pokemon">
-                        <p>Abilities</p>
-                        <ul>
-                            <li>
-                                Inner Focus
-                            </li>
-                            <li>
-                                Multiscale (Hidden)
-                            </li>
-                        </ul>
+                        <div className="abilities-pokemon">
+                            <h3>Abilities</h3>
+                            <ul>
+                                <li>
+                                    Inner Focus
+                                </li>
+                                <li>
+                                    Multiscale (Hidden)
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-            
-            
+
             <div className="secundary-info">
                 <div className="stats">
 
                 </div>
 
                 <div className="cards">
-
+                    <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SWSH4/SWSH4_EN_43.png" alt="" />
+                    <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SWSH4/SWSH4_EN_43.png" alt="" />
                 </div>
             </div>
         </section>
